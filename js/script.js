417d3d2107660604582ae4b1d6ed279e19b5b7d7
Nikolas Figueroa <nikolasfigueroa10@gmail.com>
2018-01-19 13:46:08 +0000

    htm;

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 //Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
+// Fork and clone the repo and set up your project workspace and link files
+// Write HTML for your project
 //Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
+// Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
+// Practice using console.log() and debugger to debug your code
+// Create your document ready function. 
+// Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
+// Display the user choice to the output screen
 //Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
+// Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
+// Display the computer choice to the output screen
+// Compare the user choice and the computer choice to determine who won. 
+// Display the user winner to the output screen 
+// Clear the input box for the next choice
 
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+
 
 //GLOBAL VARIABLES
 
@@ -28,3 +29,25 @@
 
 // DOCUMENT READY FUNCTION
 
+
+
+$(document).ready(function() {
+            // $("").click(function() {
+
+            // });
+
+            // $("#filter").click(function() {
+            //     var random = Math.floor(Math.random() * 5) + 1;
+            //     alert(random);
+
+            // });
+
+
+
+
+  alert(  $("true").val());
+            // $("#rps").click(function() {
+            //     alert($("#name").val());
+            // });
+            
+});
